da1a63d1775e05b32775bbc637cbe46dcf770ad5
doubleknd26 <doubleknd26@gmail.com>
2021-04-08 22:55:36 +0900

    refactor notifier

--- run_docker.sh
+++ run_docker.sh
@@ -11,7 +11,7 @@ echo '[INFO] type:' $type
 echo '[INFO] slack_webhook_url: ' $slack_webhook_url
 
 # build docker
-./gradlew clean :dockerBuild -Dtype=$type
+./gradlew clean :dockerBuild -Dtype=$type -Dslack_webhook_url=$slack_webhook_url
 
 # run docker
 sudo docker run -d -p 4444:4444 -p 11619:11619 --shm-size=128m -v /tmp/logs:/tmp/logs --name macro_app macro 
